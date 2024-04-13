16f4befe25f7c48d69fd083108bd3f131594ed35
wangrui <wrpromail@gmail.com>
2024-04-13 11:26:02 +0800

    mod: update annotations

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from langchain_openai import ChatOpenAI
 from google.cloud import translate_v2 as translate
 from google.oauth2.service_account import Credentials
 
-
 # 项目声明部分
 # todo: 应用名称、依赖等内容需要改为配置化并进行版本管理
 # 将需要的依赖在这里声明，modal 等 serverless 平台为你构建服务需要的运行环境镜像
@@ -23,6 +22,11 @@ image = Image.debian_slim().pip_install(*REQUIREMENTS)
 stub = Stub("filebot-poe2")
 # 声明需要使用的 volume 名称
 vol = Volume.from_name("my-volume")
+
+# 常量管理
+LANGDETECT_SUPPORT = ["en", "ja"]
+# 机器人所使用的一些API服务密钥是通过 modal 平台管理的，而其环境变量是用户定义的
+GOOGLE_SERVICE_ACCOUNT_JSON_ENV = "SERVICE_ACCOUNT_JSON"
 
 # utils 
 def generate_random_file_name():
@@ -62,11 +66,11 @@ def french_sentence_process(raw: str):
     return response.content
 
 # 调用 google translate 对于英文句子产生特定的处理
-def sentence_translate_process(raw: str):
-    service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
+def sentence_translate_process(text: str, sa_json: str = GOOGLE_SERVICE_ACCOUNT_JSON_ENV):
+    service_account_info = json.loads(os.environ[sa_json])
     credentials = Credentials.from_service_account_info(service_account_info)
     translate_client = translate.Client(credentials=credentials)
-    translation = translate_client.translate(raw, target_language="zh-CN")
+    translation = translate_client.translate(text, target_language="zh-CN")
     return translation["translatedText"]
 
 
@@ -74,7 +78,9 @@ class MyBot(fp.PoeBot):
     async def get_response(
         self, request: fp.QueryRequest
     ) -> AsyncIterable[fp.PartialResponse]:
+        # request.query 是一个数组，允许获取用户输入的上下文
         last_query = request.query[-1]
+        # 处理用户的附件输入
         if len(last_query.attachments) > 0:
             for attachment in last_query.attachments:
                 if attachment.content_type == 'text/plain':
@@ -85,7 +91,7 @@ class MyBot(fp.PoeBot):
                     continue
                 else:
                     yield fp.PartialResponse(text=f"Attachment type: {attachment.content_type} is not supported right now")
-            
+        # 处理用户的文本输入
         last_message = last_query.content
         if last_message is None or last_message == "":
             yield fp.PartialResponse(text="\nNo message to process")
@@ -93,7 +99,7 @@ class MyBot(fp.PoeBot):
         lang = detect(last_message)
         if lang == "fr":
             yield fp.PartialResponse(text=french_sentence_process(last_message))
-        if lang in ["en"]:
+        if lang in LANGDETECT_SUPPORT:
             yield fp.PartialResponse(text=sentence_translate_process(last_message))
         else:
             yield fp.PartialResponse(text=last_message)
